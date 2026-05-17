3350d9ed68490f5a77c7e6d5334526cb10383891
James <jwyork2008@gmail.com>
2026-05-17 15:06:21 -0500

    Designed navbar and made variables

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/logo.png'
+import discordIcon from '../../assets/icons/discord.png'
+
+export default function Navbar() {
+  return (
+    <>
+      <nav id='navbar'>
+        <img src={logo} id='logo'/>
+        <div id='title'>
+          <h1>N1ghtfall</h1>
+          <p>STRINOVA COMPETITIVE UNION</p>
+        </div>
+
+        <a href='https://discord.gg/rQNh4gKG2p' id='discordButton'><img src={discordIcon} />JOIN DISCORD</a>
+      </nav>
+    </>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './css/variables.css'
+import './css/global.css'
+import './css/components/navbar.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
