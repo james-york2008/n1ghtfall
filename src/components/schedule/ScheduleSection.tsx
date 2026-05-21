ce34d7ffe1c84a373b7ed8cfed168bdaa617bd0d
James <jwyork2008@gmail.com>
2026-05-21 13:07:26 -0500

    Dynamically rendered schedule section

--- src/components/schedule/ScheduleSection.tsx
+++ src/components/schedule/ScheduleSection.tsx
@@ -1,54 +1,15 @@
 import SectionCard from "../ui/Section";
-import clock from '../../assets/icons/clock.png'
+import { events } from '../../data/events'
+import Event from "./EventCard";
 
 export default function ScheduleSection () {
   return(
     <SectionCard>
     <h3>UPCOMING SCHEDULE</h3>
-    <article className="event">
-      <p className="eventDate"><span className="dateDay">24</span><br />MAY</p>
-      <div className="eventLabelContainer">
-        <h4 className="eventLabel">SCRIM NIGHT</h4>
-        <p>vs Nova Regiment</p>
-      </div>
-
-      <div className="eventTime">
-        <img src={clock}  alt=""/>
-        <p>8:00 PM<br />EST</p>
-      </div>
-
-      <p className="eventType">SCRIM</p>
-    </article>
-
-    <article className="event">
-      <p className="eventDate"><span className="dateDay">26</span><br />MAY</p>
-      <div className="eventLabelContainer">
-        <h4 className="eventLabel">PRACTICE NIGHT</h4>
-        <p>Internal Practice</p>
-      </div>
-
-      <div className="eventTime">
-        <img src={clock}  alt=""/>
-        <p>8:00 PM<br />EST</p>
-      </div>
-
-      <p className="eventType">PRACTICE</p>
-    </article>
-
-      <article className="event">
-        <p className="eventDate"><span className="dateDay">29</span><br />MAY</p>
-        <div className="eventLabelContainer">
-          <h4 className="eventLabel">TOURNAMENT</h4>
-          <p>Starlight Clash Qualifiers</p>
-        </div>
-
-        <div className="eventTime">
-          <img src={clock}  alt=""/>
-          <p>7:00 PM<br />EST</p>
-        </div>
-
-        <p className="eventType">TOURNAMENT</p>
-      </article>
+    
+    {events.map(event => (
+      <Event key={event.id} event={event} />
+    ))}
     </SectionCard>
   )
 }
---
+++ src/data/events.ts
@@ -0,0 +1,36 @@
+import type { Event } from '../types/event'
+
+export const events: Event[] = [
+  {
+    id: 'scrim-night-nova-regiment-2026-05-24',
+    dateDay: 24,
+    dateMonth: 'MAY',
+    eventLabel: 'SCRIM NIGHT',
+    eventDetails: 'vs Nova Regiment',
+    eventTime: '8:00 PM',
+    eventTimezone: 'EST',
+    eventType: 'SCRIM',
+  },
+ 
+  {
+    id: 'practice-night-internal-practice-2026-05-26',
+    dateDay: 26,
+    dateMonth: 'MAY',
+    eventLabel: 'PRACTICE NIGHT',
+    eventDetails: 'Internal Practice',
+    eventTime: '8:00 PM',
+    eventTimezone: 'EST',
+    eventType: 'PRACTICE',
+  },
+
+  {
+    id: 'tournament-starlight-clash-qualifiers-2026-05-29',
+    dateDay: 29,
+    dateMonth: 'MAY',
+    eventLabel: 'TOURNAMENT',
+    eventDetails: 'Starlight Clash Qualifiers',
+    eventTime: '7:00 PM',
+    eventTimezone: 'EST',
+    eventType: 'TOURNAMENT'
+  }
+]
