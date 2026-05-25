e755d47fdfbf6ebea27365f952705d2dff9f10b2
James <jwyork2008@gmail.com>
2026-05-25 15:37:45 -0500

    Adjusted README, and cleaned up css variables

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,8 @@
 export default function Footer() {
   return(
-    <p id="disclaimer">All instances of Strinova character artwork are property of their respective owners. Character images were sourced from the <a href="https://strinova.org/wiki/Characters">Strinova Wiki characters page</a>. If you hold rights to any artwork featured on this page and wish for it to be removed, you can contact me through <a href="mailto:jamesyork.dev@gmail.com">email</a>.</p>
+    <footer>
+      <p id="tagline">BUILT ON UNITY. DRIVEN BY AMBITION.</p>
+      <p id="disclaimer">All instances of Strinova character artwork are property of their respective owners. Character images were sourced from the <a href="https://strinova.org/wiki/Characters">Strinova Wiki characters page</a>. If you hold rights to any artwork featured on this page and wish for it to be removed, you can contact me through <a href="mailto:jamesyork.dev@gmail.com">email</a>.</p>
+    </footer>
   )
 }
